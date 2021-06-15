6dff790b65324a6aa911e6d1f4b453ee91dca2d8
NA-Siddiky <siddiky.academic@gmail.com>
2021-06-15 21:02:22 +0600

    fixing components

--- src/components/Home/Experiences/ExperienceBook.js
+++ src/components/Home/Experiences/ExperienceBook.js
@@ -13,46 +13,61 @@ import ielts from "../../../images/Certificate/IELTS_Certificate-1.jpg"
 
 const ExperienceBook = () => {
     return (
-        <div className="flex justify-center p-3">
-            <HTMLFlipBook width={600} height={700}>
-                <div className="bg-blue-400 shadow-md">
-                    <h2>Welcome to my Certificate Album</h2>
-                </div>
-                <div className="bg-blue-400 shadow-md">
-                    <img className="p-2" src={webDevelopment} alt="" />
-                    <link rel="stylesheet" href="https://www.coursera.org/account/accomplishments/certificate/5MZCK4U5YJJZ" />
-                </div>
-                <div className="bg-blue-200 shadow-md">
-                    <img className="p-2" src={python} alt="" />
-                    <link rel="stylesheet" href="https://www.coursera.org/account/accomplishments/certificate/GM63CYXYW9GQ" />
-                </div>
-                <div className="bg-blue-300 shadow-md">
-                    <img className="p-2" src={git} alt="" />
-                </div>
-                <div className="bg-blue-200 shadow-md">
-                    <img className="p-2" src={command} alt="" />
-                </div>
-                <div className="bg-blue-500 shadow-md">
-                    <img className="p-2" src={lict} alt="" />
-                </div>
-                <div className="bg-blue-200 shadow-md">
-                    <img className="p-2" src={workshop} alt="" />
-                </div>
-                <div className="bg-blue-300 shadow-md">
-                    <img className="p-2" src={BSC} alt="" />
-                </div>
-                <div className="bg-blue-200 shadow-md">
-                    <img className="p-2" src={BCS} alt="" />
-                </div>
-                <div className="bg-blue-500 shadow-md">
-                    <img className="p-2" src={ielts} alt="" />
-                </div>
-                <div className="bg-blue-500 shadow-md">
-                    <h1>Thank You</h1>
-                </div>
+        <section className="p-3">
 
-            </HTMLFlipBook>
-        </div>
+            <h1 className="text-center my-5 section-heading">MY CERTIFICATES</h1>
+
+            <div className="flex justify-center">
+
+                <HTMLFlipBook width={400} height={500} className="flipBook">
+                    <div className="bg-blue-400 shadow-md h-100 d-flex align-items-center text-center justify-content-center flip-book-text">
+                        <h2>Welcome to my <br />Certificate Album</h2>
+                    </div>
+                    <div className="bg-blue-400 shadow-md flip-book-img-container">
+                        <img className="p-2" src={webDevelopment} alt="" />
+                        <a href="https://www.coursera.org/account/accomplishments/certificate/5MZCK4U5YJJZ" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-200 shadow-md flip-book-img-container">
+                        <img className="p-2" src={python} alt="" />
+                        <a href="https://www.coursera.org/account/accomplishments/certificate/GM63CYXYW9GQ" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-300 shadow-md flip-book-img-container">
+                        <img className="p-2" src={git} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-200 shadow-md flip-book-img-container">
+                        <img className="p-2" src={command} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-500 shadow-md flip-book-img-container">
+                        <img className="p-2" src={lict} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-200 shadow-md flip-book-img-container">
+                        <img className="p-2" src={workshop} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-300 shadow-md flip-book-img-container">
+                        <img className="p-2" src={BSC} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-200 shadow-md flip-book-img-container">
+                        <img className="p-2" src={BCS} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-500 shadow-md flip-book-img-container">
+                        <img className="p-2" src={ielts} alt="" />
+                        <a href="#" target="_blank" className="flip-book-overlay">See Credentials</a>
+                    </div>
+                    <div className="bg-blue-500 shadow-md flip-book-img-container h-100 d-flex align-items-center text-center justify-content-center flip-book-text">
+                        <h2>Thank You</h2>
+                    </div>
+
+                </HTMLFlipBook>
+
+
+            </div>
+        </section>
     );
 };
 
