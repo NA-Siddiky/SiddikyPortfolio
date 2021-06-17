266c8176c1c11871d0a0a959a4af3cac98072d4b
NA-Siddiky <siddiky.academic@gmail.com>
2021-06-18 02:24:15 +0600

    fixing view

--- src/components/Header/HeaderBody/HeaderBody.js
+++ src/components/Header/HeaderBody/HeaderBody.js
@@ -7,10 +7,10 @@ const HeaderBody = () => {
     return (
         <div className="row d-flex w-100 m-0 p-0 pt-5 styleOfHeader">
             {/* <ParticlesBg type="circle" bg={true} /> */}
-            <div className="col-md-4 col-sm-12 d-flex h-100 justify-content-center align-items-center">
+            <div className="col-md-7 col-sm-12 d-flex h-100 justify-content-center align-items-center">
                 <HeaderMessage></HeaderMessage>
             </div>
-            <div className="col-md-8 col-sm-12 h-100 text-center justify-content-center align-items-end">
+            <div className="col-md-5 col-sm-12 h-100 text-center justify-content-center align-items-end">
                 <div className="mb-auto h-100 justify-content-center align-items-end">
 
                     <img className="headerProfileImage img-fluid" src={profile} alt="" />
