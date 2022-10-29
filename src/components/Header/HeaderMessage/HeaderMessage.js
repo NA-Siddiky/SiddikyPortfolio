58f8def2ae6b0ad31ad811c30496f77e79c1daba
NA-Siddiky <siddiky.academic@gmail.com>
2022-10-29 12:23:21 +0600

    set skils done

--- src/components/Header/HeaderMessage/HeaderMessage.js
+++ src/components/Header/HeaderMessage/HeaderMessage.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './HeaderMessage.css';
-import Typewriter from "typewriter-effect";
+import React from "react";
+import { Button } from "react-bootstrap";
 import styled from "styled-components";
-import { Button } from 'react-bootstrap';
-
+import Typewriter from "typewriter-effect";
+import "./HeaderMessage.css";
 
 const MyTitleMessage = styled.h2`
   width: 100%;
   z-index: 1;
   text-align: center;
   strong {
-    font-size: 1.00em;
+    font-size: 1em;
   }
   div {
     color: orange;
@@ -26,20 +25,31 @@ const MyTitleMessage = styled.h2`
   }
 `;
 
-
 const HeaderMessage = () => {
   return (
     <MyTitleMessage>
       <div className="titleMessage w-100 h-100 d-flex justify-content-center align-items-center">
         <div className="heading p-4 text-left w-75">
           <div className="main">
-            <h1 className="fw-bolder text-dark">Hi, I'M <br></br> <span className="color-primary my-name">N.A Siddiky</span></h1><br />
-            <h3 className="text-dark">MERN Stack Developer</h3>
+            <h1 className="fw-bolder text-dark">
+              Hi, I'M <br></br>{" "}
+              <span className="color-primary my-name">N.A Siddiky</span>
+            </h1>
+            <br />
+            <h3 className="text-dark">Full Stack Developer</h3>
           </div>
           <div className="sub text-warning">
             <Typewriter
               options={{
-                strings: ["React", "Node Js", "Redux", "MongoDB", "Express JS"],
+                strings: [
+                  "React",
+                  "Redux",
+                  "Node Js",
+                  "Express JS",
+                  "MySQL",
+                  "Sequelize",
+                  "MongoDB",
+                ],
                 autoStart: true,
                 loop: true,
                 delay: 50,
@@ -47,7 +57,13 @@ const HeaderMessage = () => {
             />
           </div>
           <div>
-            <Button className="btn-custom mt-3" target="_blank" href="https://drive.google.com/file/d/1MD7KtIoMcxQtXTMzjNUkHx3ow49S_k8_/view?usp=sharing">RESUME</Button>
+            <Button
+              className="btn-custom mt-3"
+              target="_blank"
+              href="https://drive.google.com/file/d/1MD7KtIoMcxQtXTMzjNUkHx3ow49S_k8_/view?usp=sharing"
+            >
+              RESUME
+            </Button>
           </div>
         </div>
       </div>
@@ -56,6 +72,3 @@ const HeaderMessage = () => {
 };
 
 export default HeaderMessage;
-
-
-
--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,37 +1,51 @@
-import React from 'react';
-import profile from '../../../images/Photo 2.jpg'
-import './About.css'
+import React from "react";
+import profile from "../../../images/Photo 2.jpg";
+import "./About.css";
 
 const About = () => {
-    return (
-        <section className="w-100 p-5 styleOfAbout" id="about">
-            <div className="container mt-5 about-section d-flex flex-wrap">
-                <div className="sectionAbout">
-                </div>
+  return (
+    <section className="w-100 p-5 styleOfAbout" id="about">
+      <div className="container mt-5 about-section d-flex flex-wrap">
+        <div className="sectionAbout"></div>
 
-                <div className="row aboutBox p-4">
-                    <div className="col-md-5 about-img d-flex justify-content-center">
-                        <div className="about-img-border">
-                            <img className="profileImage" src={profile} alt="" />
-                        </div>
-
-                    </div>
-
-                    <div className="col-md-7 about-info text-justify">
-                        <h1 className="section-heading my-3">About Me</h1>
-                        <h4>MERN STACK DEVELOPER</h4>
-
-                        <p><span>By conjoining hard work and hope you will get a name <strong>"Siddiky"</strong>.</span> I’m desirous to develop in a challenging workplace that welcomes innovative ideas and offers growth opportunities and a positive
-environment as a leader or team member.</p>
-
-                        <p>It’s my passion to work on real-time web applications using JavaScript (ES6,
-React, Next) & Node.js is really awesome backend technology that I always prefer.</p>
-                        <a className="btn btn-custom" href="#contact">HIRE ME</a>
-                    </div>
-
-                </div>
+        <div className="row aboutBox p-4">
+          <div className="col-md-5 about-img d-flex justify-content-center">
+            <div className="about-img-border">
+              <img className="profileImage" src={profile} alt="" />
             </div>
-        </section >
-    );
+          </div>
+
+          <div className="col-md-7 about-info text-justify">
+            <h1 className="section-heading my-3">About Me</h1>
+            <h4>FULL STACK DEVELOPER</h4>
+
+            <p>
+              <span>
+                By conjoining hard work and hope you will get the name{" "}
+                <strong>"Siddiky"</strong>.
+              </span>{" "}
+              I would like to grow professionally in an organization that values
+              innovative ideas, offers growth opportunities in a positive work
+              environment as a leader or team member.
+            </p>
+
+            <p>
+              Competitive programming and problem-solving are the things that I
+              love most. It is my passion to create real-time applications using
+              JavaScript, <strong>React</strong>, and Redux including the
+              backend technology
+              <strong> node.js</strong>, express.js. And I always prefer{" "}
+              sequelize or Prisma for ORMs since I am comfortable working with
+              both <strong>MySQL</strong> and <strong>MongoDB</strong> in
+              projects.
+            </p>
+            <a className="btn btn-custom" href="#contact">
+              HIRE ME
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default About;
