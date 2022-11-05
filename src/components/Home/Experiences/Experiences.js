93f2300e214b9692aa49be94f296e1b07faaa25f
NA-Siddiky <siddiky.academic@gmail.com>
2022-11-05 12:24:49 +0600

    experience box padding fixing

--- src/components/Home/Experiences/Experiences.js
+++ src/components/Home/Experiences/Experiences.js
@@ -10,9 +10,9 @@ const Experiences = () => {
         <Card.Img
           variant="top"
           src="https://yt3.ggpht.com/ytc/AMLnZu_JuiwUKA8OSoT97avCn9xufJV0LsQFYJkMYdAq=s900-c-k-c0x00ffffff-no-rj"
-          className="d-block m-auto experienceInfo"
+          className="d-block m-auto experienceInfo p-2"
         />
-        <Card.Body>
+        <Card.Body className="p-1">
           <Card.Title className="experience-header-title"><h5>Company Name: Programming Hero</h5></Card.Title>
           <Card.Title className="experience-header-title">
             <h6>Full Stack Developer (June 2021 to present)</h6>
